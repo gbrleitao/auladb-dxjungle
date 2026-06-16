259af34025acf050610fce6e5193ee4c947d8eda
Gabriel Leitao <gabriel.leitao@icomp.ufam.edu.br>
2026-06-16 10:19:39 -0400

    atualização banco.py
    criação das funções conectar() e adição do with

---
+++ src/banco.py
@@ -0,0 +1,35 @@
+import sqlite3
+from pathlib import Path
+
+CAMINHO_BANCO = Path("dados/aula-01.db")
+CAMINHO_BANCO.parent.mkdir(exist_ok=True)
+
+def conectar():
+    conexao = sqlite3.connect(CAMINHO_BANCO)
+    conexao.execute("PRAGMA foreign_keys = ON")
+    return conexao
+
+with conectar() as conn:
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS turmas(
+                id_turma INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                curso TEXT NOT NULL,
+                ano INTEGER NOT NULL
+                );
+    """)
+    conn.commit()
+
+nome = input("Nome da turma: ")
+curso = input("Curso: ")
+ano = int(input("Ano: "))
+
+with conectar() as conn:
+    conn.execute("""
+        INSERT INTO turmas(nome, curso, ano)
+        VALUES(?,?,?)""",
+        (nome,curso,ano)
+    )
+    conn.commit()
+
+conn.close()
